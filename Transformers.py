a08dd984ee7a421c0ae34e402c9194da58858e02
nicolas <nico@MacBook-Pro-de-nicolas.local>
2020-07-05 17:35:45 -0300

    Add transformer py

---
+++ Transformers.py
@@ -0,0 +1,161 @@
+#!/usr/bin/env python
+# coding: utf-8
+from torch.utils.data import Dataset, DataLoader
+import torch
+import pandas as pd
+from transformers import AutoTokenizer, AutoModelWithLMHead
+from sklearn.metrics import classification_report, accuracy_score, balanced_accuracy_score
+from sklearn.model_selection import train_test_split
+
+
+# In[ ]:
+
+
+df = pd.read_csv('data/train_clean.csv')
+df = df[['clean_txt', 'Intencion']]
+df.columns = ['Pregunta', 'Intencion']
+
+
+encode_dict = {}
+def encode_cat(x):
+    if x not in encode_dict.keys():
+        encode_dict[x]=len(encode_dict)
+    return encode_dict[x]
+
+df['ENCODE_CAT'] = df['Intencion'].apply(lambda x: encode_cat(x))
+NB_CLASS = len(encode_dict)
+
+# Defining some key variables that will be used later on in the training
+MAX_LEN = 512
+TRAIN_BATCH_SIZE = 16
+VALID_BATCH_SIZE = 16
+EPOCHS = 5
+LEARNING_RATE = 1e-05
+tokenizer = AutoTokenizer.from_pretrained("dccuchile/bert-base-spanish-wwm-cased")
+
+class Triage(Dataset):
+    def __init__(self, dataframe, tokenizer, max_len):
+        self.len = len(dataframe)
+        self.data = dataframe
+        self.tokenizer = tokenizer
+        self.max_len = max_len
+        
+    def __getitem__(self, index):
+        pregunta = str(self.data.Pregunta[index])
+        pregunta = " ".join(pregunta.split())
+        inputs = self.tokenizer.encode_plus(
+            pregunta,
+            None,
+            add_special_tokens=True,
+            max_length=self.max_len,
+            truncation=True,
+            pad_to_max_length=True,
+            return_token_type_ids=True
+        )
+        ids = inputs['input_ids']
+        mask = inputs['attention_mask']
+        token_type_ids = inputs["token_type_ids"]
+
+        return {
+            'ids': torch.tensor(ids, dtype=torch.long),
+            'mask': torch.tensor(mask, dtype=torch.long),
+            'token_type_ids': torch.tensor(token_type_ids, dtype=torch.long),
+            'targets': torch.tensor(self.data.ENCODE_CAT[index], dtype=torch.long)
+        } 
+    
+    def __len__(self):
+        return self.len
+
+# Creating the dataset and dataloader for the neural network
+train_dataset=df.reset_index().drop(columns="index")
+
+print("FULL Dataset: {}".format(df.shape))
+
+
+training_set = Triage(train_dataset, tokenizer, MAX_LEN)
+
+train_params = {'batch_size': TRAIN_BATCH_SIZE,
+                'shuffle': True,
+                'num_workers': 0
+                }
+
+
+training_loader = DataLoader(training_set, **train_params)
+
+
+class BERTClass(torch.nn.Module):
+    def __init__(self):
+        super(BERTClass, self).__init__()
+        self.l1 = AutoModelWithLMHead.from_pretrained("dccuchile/bert-base-spanish-wwm-cased").base_model
+        self.l2 = torch.nn.Dropout(0.3)
+        self.l3 = torch.nn.Linear(768, NB_CLASS)
+    
+    def forward(self, ids, mask, token_type_ids):
+        _, output_1= self.l1(ids, attention_mask=mask, token_type_ids=token_type_ids)
+        output_2 = self.l2(output_1)
+        output = self.l3(output_2)
+        return output
+
+
+device = 'cuda' # 'cpu'
+model = BERTClass()
+model.to(device)
+
+loss_function = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(params=model.parameters(), lr=LEARNING_RATE)
+
+def train(epoch):
+    model.train()
+    for _, data in enumerate(training_loader, 0):
+        ids = data['ids'].to(device, dtype = torch.long)
+        mask = data['mask'].to(device, dtype = torch.long)
+        targets = data['targets'].to(device, dtype = torch.long)
+        token_type_ids = data['token_type_ids'].to(device, dtype = torch.long)
+
+        outputs = model(ids, mask, token_type_ids)
+
+        optimizer.zero_grad()
+        loss = loss_function(outputs, targets)
+        if _%100==0:
+            print(f'Epoch: {epoch}, Loss:  {loss.item()}')
+        
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+def valid(model, testing_loader):
+    model.eval()
+    y_true = []
+    y_pred = []
+    n_correct = 0; n_wrong = 0; total = 0
+    with torch.no_grad():
+        for _, data in enumerate(testing_loader, 0):
+            ids = data['ids'].to(device, dtype = torch.long)
+            mask = data['mask'].to(device, dtype = torch.long)
+            token_type_ids = data['token_type_ids'].to(device, dtype = torch.long)
+            
+            targets = data['targets'].to(device, dtype = torch.long)
+            outputs = model(ids, mask, token_type_ids)
+            big_val, big_idx = torch.max(outputs.data, dim=1)
+            total+=targets.size(0)
+            n_correct+=(big_idx==targets).sum().item()
+            y_true += targets
+            y_pred += big_idx
+    return y_true, y_pred, (n_correct*100.0)/total
+
+
+for epoch in range(EPOCHS):
+    train(epoch)
+    y_t, y_p, acc = valid(model, training_loader)
+	print("Accuracy on test data = %0.2f%%" % acc)
+
+
+output_model_file = './pytorch_beto_news_clean.bin'
+output_vocab_file = './vocab_beto_news_clean.bin'
+model_to_save = model
+torch.save(model_to_save, output_model_file)
+tokenizer.save_vocabulary(output_vocab_file)
+
+print('All files saved')
+print('This tutorial is completed')
+
