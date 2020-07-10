88c8116748cc4fca2f43dab331e8b326577990a3
nicolas <nico@ip-192-168-169-99.us-west-2.compute.internal>
2020-07-09 21:58:47 -0300

    ensemble update

--- ensemble.py
+++ ensemble.py
@@ -14,7 +14,19 @@ ENSEMBLES = {
                   "submits/submit_transfomer_0.8145_lb.csv",  # without weights;without DA
                   "submits/submit_transfomer_0.846.csv"],  # with weights; with DA (es,en)
         "output": "submits/submit_ensemble_nb_1.csv",  # lb 0.850
-        "gold": 2}
+        "gold": 2},
+    2: {"input": ["submits/preprocess_countv_tfidf_svc_df_0.71.csv",
+                  "submits/submit_transfomer_0.8422.csv",  # with weights;with Partial DA (es, en, fr)
+                  "submits/submit_transfomer_0.846.csv"],  # with weights; with DA (es,en)
+        "output": "submits/submit_ensemble_nb_2.csv",  # lb 0.8534
+        "gold": 2},
+    3: {"input": ["submits/countv_tfidf_svc_df_0.68_lb.csv",
+                  "submits/preprocess_countv_tfidf_svc_df_0.71.csv",
+                  "submits/submit_transfomer_0.8293.csv",  # with weights;with Partial DA (es, en, fr, pt, ar)
+                  "submits/submit_transfomer_0.8422.csv",  # with weights;with Partial DA (es, en, fr)
+                  "submits/submit_transfomer_0.846.csv"],  # with weights; with DA (es,en)
+        "output": "submits/submit_ensemble_nb_3.csv",  # lb 0.8567
+        "gold": 4}
 }
 
 
@@ -31,7 +43,7 @@ def ensemble(preds_f, output_path, gold, option):
         final_preds = mode[0]
         counts = counts[0]
 
-        to_gold_idxs = list(np.argwhere(counts == 1).reshape(1, -1)[0])
+        to_gold_idxs = list(np.argwhere((counts == 1) | (counts == 2)).reshape(1, -1)[0])
 
         final_preds[to_gold_idxs] = gold_preds[to_gold_idxs]
 
@@ -40,7 +52,7 @@ def ensemble(preds_f, output_path, gold, option):
 
 
 if __name__ == "__main__":
-    ens = 0
+    ens = 3
     ensemble(ENSEMBLES[ens]["input"],
              ENSEMBLES[ens]["output"],
              gold=ENSEMBLES[ens]["gold"],
